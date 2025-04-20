c6e7b4816f369be01c909f75122bd9bb9c8baac7
ZCaks <zan.caks@gmail.com>
2025-04-20 13:16:33 +0200

    Add verify-email.html to Vercel config

--- api/server.js
+++ api/server.js
@@ -77,6 +77,7 @@ app.get("/api/maps-loader", (req, res) => {
     const script = document.createElement('script');
     script.src = "https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places";
     script.async = true;
+    script.defer = true;
     document.head.appendChild(script);
   `);
 });
